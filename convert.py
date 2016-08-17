38b1672490bde5427a74be1c1c51e43434f2e596
Nadja Deininger <nadja@ef.gy>
2016-08-17 19:49:25 +0100

    use a lookup list for the ASCII characters

--- convert.py
+++ convert.py
@@ -4,24 +4,20 @@ import numpy as np
 
 from PIL import Image
 
-def image_to_ascii(image, n_colors=3):
-    def to_char(c):
-        if c == 0:
-            return ' '
-        elif c == 1:
-            return '-'
-        else:
-            return 'X'
+lookup = " .,:-?X#"
+
+
+def image_to_ascii(image):
     """
     PIL image object -> 2d array of values
 
     """
-    quantised = image.quantize(n_colors)
+    quantised = image.quantize(len(lookup))
 
     quantised.show()
     array = np.asarray(quantised.resize((128,64)))
 
-    return [[to_char(k) for k in i] for i in array] 
+    return [[lookup[k] for k in i] for i in array] 
 
 
 

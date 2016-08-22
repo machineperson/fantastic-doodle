aa974a2d12020e324db222b022594d9e489e559f
Nadja Deininger <nadja@ef.gy>
2016-08-22 19:58:06 +0100

    CORRECT height/width conversion this time around

--- convert.py
+++ convert.py
@@ -4,18 +4,28 @@ import numpy as np
 
 from PIL import Image
 
-lookup = " .,:-?X#"
+lookup = " .,:-!?X#"
 
-
-def image_to_ascii(image):
+def image_to_ascii(image, width=128):
     """
     PIL image object -> 2d array of values
 
     """
+    def scale_height(h, w, new_width):
+        print "original height: {}".format(h)
+        print "original width: {}".format(w)
+        print "new width: {}".format(new_width)
+        conversion_factor = float(1.0 * new_width / w)
+        print "conversion factor {}".format(conversion_factor)
+        new_height = (h * conversion_factor) / 2.0
+        print "new height: {}".format(new_height)
+        return int(new_height)
+
+
     quantised = image.quantize(len(lookup))
 
-    quantised.show()
-    array = np.asarray(quantised.resize((128,64)))
+    #quantised.show()
+    array = np.asarray(quantised.resize((width, scale_height(image.height, image.width, width))))
 
     return [[lookup[k] for k in i] for i in array] 
 

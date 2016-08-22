1fe63ed779eae63cd3531122a5c6c897431acbd5
Nadja Deininger <nadja@ef.gy>
2016-08-22 20:38:14 +0100

    optional width argument

--- convert.py
+++ convert.py
@@ -4,7 +4,8 @@ import numpy as np
 
 from PIL import Image
 
-lookup = " .,:-!?X#"
+lookup = " .:-=+*#%@"
+
 
 def image_to_ascii(image, width=128):
     """
@@ -27,15 +28,18 @@ def image_to_ascii(image, width=128):
     #quantised.show()
     array = np.asarray(quantised.resize((width, scale_height(image.height, image.width, width))))
 
+    return array_to_ascii(array) 
+
+
+def array_to_ascii(array):
     return [[lookup[k] for k in i] for i in array] 
 
 
-
-def convert_file(fn):
+def convert_file(fn, width):
     converted = ""
     try:
         image = Image.open(fn)
-        converted = image_to_ascii(image)
+        converted = image_to_ascii(image, width)
     except Exception as e:
         print e.message
 
@@ -48,8 +52,12 @@ if __name__ == '__main__':
     parser.add_argument("-f", "--filename", 
                         help="Convert this file to ASCII art", 
                         required=True)
+    parser.add_argument("-w", "--width", 
+                        help="Width of the new image (default 128)", 
+                        type=int,
+                        default=128)
     args = parser.parse_args()
     print args
     filename = args.filename
-    converted = convert_file(filename)
+    converted = convert_file(filename, args.width)
     print '\n'.join(''.join(i) for i in converted)
